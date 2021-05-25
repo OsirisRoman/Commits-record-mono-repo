0a098ad136f1d06655b3b9dc21dba3889f267ca0
Osiris Román <osirisr1994@gmail.com>
2021-05-25 12:15:22 -0500

    Updating from var to const & Adding controllers.

--- api/app.js
+++ api/app.js
@@ -1,12 +1,10 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const indexRouter = require("./routes/index");
 
-var app = express();
+const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -15,6 +13,5 @@ app.use(cookieParser());
 app.use(express.static("../app/build"));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 module.exports = app;
